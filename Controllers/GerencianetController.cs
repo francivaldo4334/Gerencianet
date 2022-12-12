d8fbe463aadb74e06ee1336fdd3e41a9918d1b61
franc_000 <francivaldodev@gmail.com>
2022-12-12 11:33:03 -0300

    correcoes

--- Controllers/GerencianetController.cs
+++ Controllers/GerencianetController.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Cryptography.X509Certificates;
-using System.Net;
 using RestSharp;
 
 namespace OfficesafeAndGerencianet.Controllers;
@@ -8,45 +7,38 @@ namespace OfficesafeAndGerencianet.Controllers;
 [Route("[controller]")]
 public class GerencianetController:ControllerBase{
     public static string Base64Encode(string plainText)
-        {
-            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
-            return System.Convert.ToBase64String(plainTextBytes);
-        }
-    [HttpGet]
-    public async Task<string> AutoraizerAsync()
     {
-            var credencials = new Dictionary<string, string>{
-                {"client_id", "Client_Id_..."},
-                {"client_secret", "Client_Secret_..."}
-            };
-            var authorization = Base64Encode(credencials["client_id"] + ":" + credencials["client_secret"]);
-            var certificates = new X509CertificateCollection();
-            certificates.Add(new X509Certificate2("./assets/certificado.p12", ""));
-            var options = new RestClientOptions("https://apis.gerencianet.com.br/oauth/token") {
-                ThrowOnAnyError = true,
-                ClientCertificates = certificates
-            };
-            var client = new RestClient(options);
-            var request = new RestRequest(new Uri("https://api.github.com/users/francivaldo4334"),Method.Get);
-
-            request.AddHeader("Authorization", "Basic " + authorization);
-            request.AddHeader("Content-Type", "application/json");
-            request.AddParameter("application/json", "{\r\n    \"grant_type\": \"client_credentials\"\r\n}", ParameterType.RequestBody);
-            
-            RestResponse restResponse = await client.ExecuteAsync(request);
-            string response = restResponse.Content;
-
-            Console.WriteLine($"RES : {response}");
-            return response;
+        var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+        return System.Convert.ToBase64String(plainTextBytes);
     }
     [HttpGet]
-    [Route("Acess")]
+    [Route("Autorizer")]
+    public async Task<string> Autoraizer()
+    {
+        var credencials = new Dictionary<string, string>{
+            {"client_id", "you_Client_Id"},
+            {"client_secret", "you_Client_Secret_"}
+        };
+        var authorization = Base64Encode(credencials["client_id"] + ":" + credencials["client_secret"]);
+        HttpClientHandler handler = new HttpClientHandler();
+        handler.ClientCertificates.Add(new X509Certificate2("./assets/certificado.p12", ""));
+        var client = new RestClient(handler);
+        var request = new RestRequest(new Uri("https://apis.gerencianet.com.br/open-finance/oauth/token"),Method.Post);
+        request.AddHeader("Authorization", "Basic " + authorization);
+        request.AddHeader("Content-Type", "application/json");
+        request.AddParameter("application/json", "{\r\n    \"grant_type\": \"client_credentials\"\r\n}", ParameterType.RequestBody);
+        RestResponse restResponse = await client.ExecuteAsync(request);
+        string response = restResponse.Content;
+        return response;
+    }
+    [HttpGet]
+    [Route("Access")]
     public async Task<string> Acess([FromQuery]string path)
     {
-            var client = new RestClient();
-            var request = new RestRequest(new Uri(path),Method.Get);
-            RestResponse restResponse = await client.ExecuteAsync(request);
-            string response = restResponse.Content;
-            return response;
+        var client = new RestClient();
+        var request = new RestRequest(new Uri(path),Method.Get);
+        RestResponse restResponse = await client.ExecuteAsync(request);
+        string response = restResponse.Content;
+        return response;
     }
 }
